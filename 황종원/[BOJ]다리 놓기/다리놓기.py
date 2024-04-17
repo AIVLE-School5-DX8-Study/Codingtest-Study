d433040525ae3001b34185e4055c104f54dedd02
jongwon <hjw0982@naver.com>
2024-04-17 20:09:23 +0900

    commit

---
+++ 황종원/[BOJ]다리 놓기/다리놓기.py
@@ -0,0 +1,13 @@
+import math
+
+def build_bridge(N, M):
+    count = math.factorial(M) // (math.factorial(N) * math.factorial(M - N))
+    return count
+
+T = int(input())
+
+for _ in range(T):
+    N, M = map(int, input().split())
+    result = build_bridge(N, M)
+    print(result)
+
