0e1b98670b31866a96c3a9ba914f87994346ec88
Seungbin <hsbin72@gmail.com>
2024-05-04 01:57:28 +0900

    {PGS] / Lv.2 / 타겟 넘버 / 50min

---
+++ 허승빈/[PGS]타겟 넘버/[PGS]타겟 넘버.py
@@ -0,0 +1,17 @@
+def solution(numbers, target):
+    suum = 0
+    visits = [0]
+    for i in numbers:
+        ans = []
+    
+        for visit in visits:
+            ans.append(visit-i)
+            ans.append(visit+i)
+
+        visits = ans
+    for visit in visits:
+        if visit == target:
+            suum += 1
+            
+        
+    return suum
